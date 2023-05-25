7864728e63c3b4be4552970b42fdab3af7054295
Evgeny Kozlov <kozloveg1996@gmail.com>
2023-05-25 12:39:21 +0300

    feat: Add recursive max list element algorithm

---
+++ RecursiveMaxCountInList/RecursiveMaxCountInList/MaxElementCalculator.cs
@@ -0,0 +1,23 @@
+﻿namespace RecursiveMaxCountInList
+{
+	public static class MaxElementCalculator
+	{
+		public static int? GetMaxElement(IList<int> ints)
+		{
+			if(ints == null || !ints.Any())
+			{
+				return null;
+			}
+
+			if(ints.Count() == 1)
+			{
+				return ints[0];
+			}
+
+			ints.Remove(ints.Min());
+
+			return GetMaxElement(ints);
+		}
+	}
+}
+
---
+++ RecursiveMaxCountInList/RecursiveMaxCountInListTests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using RecursiveMaxCountInList;
+
+namespace RecursiveMaxCountInListTests;
+
+public class Tests
+{
+    private static readonly object[] _sourceLists =
+    {
+        new object[] {new List<int> {1}},   //case 1
+        new object[] {new List<int> {1, 2}}, //case 2
+        new object[] {new List<int> { 1, 1,} },
+        new object[] {new List<int> { 9, 8, 1} },
+        new object[] {new List<int> { -1, -2, -4} }
+    };
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TestCaseSource(nameof(_sourceLists))]
+    public void MaxElementCalculator_Correct_For_Common_Lists(IList<int> ints)
+    {
+        Assert.That(MaxElementCalculator.GetMaxElement(ints), Is.EqualTo(ints.Max()));
+    }
+
+    [Test]
+    public void MaxElementCalculator_Corrent_For_Empty_List()
+    {
+        Assert.That(MaxElementCalculator.GetMaxElement(new List<int>()), Is.Null);
+    }
+
+    [Test]
+    public void MaxElementCalculator_Corrent_For_Null()
+    {
+        Assert.That(MaxElementCalculator.GetMaxElement(null), Is.Null);
+    }
+}
