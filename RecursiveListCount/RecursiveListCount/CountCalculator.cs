66441934073fa655985d3c5f94be8ede1b1f6dba
Evgeny Kozlov <kozloveg1996@gmail.com>
2023-05-25 12:29:03 +0300

    feat: Add recursive length calculator

---
+++ RecursiveListCount/RecursiveListCount/CountCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace RecursiveListCount
+{
+	public static class CountCalculator
+	{
+		public static int CalculateLength(IList<object> objects)
+		{
+			if(objects == null)
+			{
+				return -1;
+			}
+
+			if (!objects.Any())
+			{
+				return 0;
+			}
+
+			objects.RemoveAt(0);
+			return 1 + CalculateLength(objects);
+		}
+	}
+}
+
---
+++ RecursiveListCount/RecursiveListCountTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿namespace RecursiveListCountTest;
+
+public class Tests
+{
+    private static readonly object[] _sourceLists =
+    {
+        new object[] {new List<object> {1}},   //case 1
+        new object[] {new List<object> {1, 2}},
+        new object[] {new List<object>() }
+
+    };
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TestCaseSource(nameof(_sourceLists))]
+    public void CountCalculator_Correct_Calculate_Common_Lists(IList<object> objects)
+    {
+        var objectsCount = objects.Count();
+        Assert.That(RecursiveListCount.CountCalculator.CalculateLength(objects), Is.EqualTo(objectsCount));
+    }
+
+    [Test]
+    public void CountCalculator_Correct_Calculate_Null()
+    {
+        Assert.That(RecursiveListCount.CountCalculator.CalculateLength(null), Is.EqualTo(-1));
+    }
+}
