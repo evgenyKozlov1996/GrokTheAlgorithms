ef0cdd92ae49691ce29dc48656a8e5d5014f3a06
Evgeny Kozlov <kozloveg1996@gmail.com>
2023-05-25 13:09:36 +0300

    feat: Add quick sort

---
+++ QuickSort/QuickSort/QuickSorter.cs
@@ -0,0 +1,28 @@
+﻿namespace QuickSort
+{
+	public static class QuickSorter
+	{
+		public static IList<int> QuickSort(IList<int> ints)
+		{
+			if(ints == null || ints.Count == 0 || ints.Count == 1)
+			{
+				return ints;
+			}
+
+			var pivot = ints[0];
+
+			ints.RemoveAt(0);
+
+			var lessThanPivot = ints.Where(num => num < pivot).ToList();
+			var moreThanPivot = ints.Where(num => num >= pivot).ToList();
+
+			var sortedLess = QuickSort(lessThanPivot);
+			var sortedMore = QuickSort(moreThanPivot);
+
+			sortedLess.Add(pivot);
+			return sortedLess.Concat(sortedMore).ToList();
+
+		}
+	}
+}
+
---
+++ QuickSort/QuickSortTests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using QuickSort;
+
+namespace QuickSortTests;
+
+public class Tests
+{
+    private static readonly object[] _sourceLists =
+    {
+        new object[] {new List<int> {1}},
+        new object[] {new List<int> {2, 1}},
+        new object[] {new List<int> ()},
+        new object[] {new List<int> { -1, -2, -3, 4, 3, 5, 9, 10} }
+    };
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TestCaseSource(nameof(_sourceLists))]
+    public void QuickSorter_Correct_Sort_Common_List(IList<int> ints)
+    {
+        // Arrange
+        var listCopy = ints.Select(num => num).ToList();
+        listCopy.Sort();
+
+        // Act 
+        var sortedList = QuickSorter.QuickSort(ints);
+
+        // Assert
+        Assert.That(sortedList, Is.EquivalentTo(listCopy));
+    }
+
+    [Test]
+    public void QuickSorter_Correct_Sort_Null()
+    {
+        Assert.That(QuickSorter.QuickSort(null), Is.Null);
+    }
+}
