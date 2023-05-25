58602fd63d3ec1fd0fe3c36f0b6517648f07c5a1
Evgeny Kozlov <kozloveg1996@gmail.com>
2023-05-25 18:49:13 +0300

    feat: Add Dijkstra search algorithm

---
+++ DijkstraSearch/DijkstraSearch/DijkstraSearcher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace DijkstraSearch
+{
+	public static class DijkstraSearcher
+	{
+		public static void CalculatePathCost(IDictionary<string, Dictionary<string, int>> graph, IDictionary<string, int> costs, IDictionary<string, string> parent)
+		{
+			var processedNodes = new List<string>();
+			var node = FindLowestCostNode(costs, processedNodes);
+			while(node != null)
+			{
+				var cost = costs[node];
+				var neighbours = graph[node];
+				foreach(var key in neighbours.Keys)
+				{
+					var newCost = cost + neighbours[key];
+					if(newCost < costs[key])
+					{
+						costs[key] = newCost;
+						parent[key] = node;
+					}
+				}
+
+				processedNodes.Add(node);
+				node = FindLowestCostNode(costs, processedNodes);
+			}
+		}
+
+		private static string? FindLowestCostNode(IDictionary<string, int> costs, IList<string> processed)
+		{
+			var lowestCost = int.MaxValue;
+			string lowestCostNode = null;
+			foreach(var node in costs.Keys)
+			{
+				var cost = costs[node];
+				if(cost < lowestCost && !processed.Contains(node))
+				{
+					lowestCost = cost;
+					lowestCostNode = node;
+				}
+			}
+
+			return lowestCostNode;
+		}
+	}
+}
+
---
+++ DijkstraSearch/DijkstraSearchTests/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using DijkstraSearch;
+
+namespace DijkstraSearchTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void DijkstraSearcher_Correct_Calculate_Min_Path_Cost()
+    {
+        // Arrange
+        var graph = new Dictionary<string, Dictionary<string, int>>();
+        graph.Add("start", new Dictionary<string, int>
+        {
+            {"a", 6 },
+            {"b", 2 }
+        });
+        graph.Add("a", new Dictionary<string, int>
+        {
+            { "fin", 1 }
+        });
+        graph.Add("b", new Dictionary<string, int>
+        {
+            { "a", 3},
+            { "fin", 5 }
+        });
+
+        var costs = new Dictionary<string, int>
+        {
+            { "a", 2 },
+            { "b", 6 },
+            { "fin", int.MaxValue }
+        };
+        Assert.Pass();
+
+        var parents = new Dictionary<string, string>
+        {
+            { "a", "start" },
+            { "b", "start" },
+            { "fin", null }
+        };
+
+        // Act
+        DijkstraSearcher.CalculatePathCost(graph, costs, parents);
+
+        // Assert
+        Assert.That(costs["fin"], Is.EqualTo(6));
+    }
+}
