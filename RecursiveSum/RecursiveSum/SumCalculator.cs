c7905d4a979aba2f47c43dce04c46c57afa327d2
Evgeny Kozlov <kozloveg1996@gmail.com>
2023-05-25 12:18:27 +0300

    feat: Add recursive sum algorithm

---
+++ RecursiveSum/RecursiveSum/SumCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace RecursiveSum
+{
+	public static class SumCalculator
+	{
+        public static int? Sum(IList<int> ints)
+        {
+            if (ints == null || ints.Count == 0)
+            {
+                return null;
+            }
+
+            var sum = 0;
+            if (ints.Count() == 1)
+            {
+                return ints.ElementAt(0);
+            }
+
+            ints.RemoveAt(0);
+            return sum + Sum(ints);
+        }
+    }
+}
+
---
+++ RecursiveSum/RecursiveSumTest/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using RecursiveSum;
+
+namespace RecursiveSumTest;
+
+public class RecursiveSumTests
+{
+    private static readonly object[] _sourceLists =
+    {
+        new object[] {new List<int> {1}},   //case 1
+        new object[] {new List<int> {1, 2, 6}}, //case 2
+        new object[] {new List<int> {1, 2, 9}}, //case 3
+
+    };
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TestCaseSource(nameof(_sourceLists))]
+    public void RecursiveSum_Calculate_Correct_With_Common_Array(IList<int> ints)
+    {
+        Assert.That(SumCalculator.Sum(ints), Is.EqualTo(ints.Sum()));
+    }
+
+    [Test]
+    public void RecursiveSum_Correct_With_Empty_List()
+    {
+        Assert.That(SumCalculator.Sum(new List<int>()), Is.Null);
+    }
+
+    [Test]
+    public void RecursiveSum_Correct_With_Null()
+    {
+        Assert.That(SumCalculator.Sum(null), Is.Null);
+    }
+}
