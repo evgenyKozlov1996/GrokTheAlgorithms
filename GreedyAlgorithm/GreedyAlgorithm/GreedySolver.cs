334fcb24dd9a62f8da5b69d540d91e17d1ea9b2c
Evgeny Kozlov <kozloveg1996@gmail.com>
2023-05-26 10:45:05 +0300

    feat: Add GreedySolver tests. TDD

---
+++ GreedyAlgorithm/GreedyAlgorithm/GreedySolver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace GreedyAlgorithm
+{
+	public static class GreedySolver
+	{
+		public static ICollection<string> SolveGreedyProblem(string[] requiredStates, IDictionary<string, string[]> stations)
+		{
+
+		}
+	}
+}
+
---
+++ GreedyAlgorithm/GreedyAlgorithmTest/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using GreedyAlgorithm;
+
+namespace GreedyAlgorithmTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void GreedySolver_Return_Correct_Answer()
+    {
+        // Arrange
+        var requiredStates = new string[] {
+            "mt", "wa", "or", "id", "nv", "ut"
+        };
+
+        var stations = new Dictionary<string, string[]>
+        {
+            {"kone", new string[] {"id", "nv", "ut"} },
+            {"ktwo", new string[] {"wa", "id", "mt"} },
+            {"kthree", new string[] {"or", "nv", "ca"} },
+            {"kfour", new string[] {"nv", "ut"} },
+            {"kfive", new string[] {"ca", "az"} }
+        };
+
+        var result = new string[] { "kone", "ktwo", "kthree", "kfive" };
+
+        //Act
+        var required = GreedySolver.SolveGreedyProblem(requiredStates, stations);
+
+        //Assert
+        Assert.That(required, Is.EquivalentTo(result));
+    }
+}
