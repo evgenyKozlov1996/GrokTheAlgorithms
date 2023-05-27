4c3e9b40c4bd996f177177237bc224d4e266582a
Evgeny Kozlov <kozloveg1996@gmail.com>
2023-05-27 20:55:04 +0300

    feat: Adde dynamic programming example

---
+++ DynamicProgramming/DynamicProgramming/StringComparisonSolver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace DynamicProgramming
+{
+    public static class StringComparisonSolver
+    {
+        public static int CalculateIsSame(string str1, string str2)
+        {
+            var comparisonMatrix = new int[str1.Length, str2.Length];
+            int maxVal = 0;
+            for (var i = 0; i < str1.Length; i++)
+            {
+                for (var j = 0; j < str2.Length; j++)
+                {
+                    if (str1[i] == str2[j])
+                    {
+                        if (i - 1 < 0 || j - 1 < 0)
+                        {
+                            comparisonMatrix[i, j] = 1;
+                        }
+                        else
+                        {
+                            comparisonMatrix[i, j] = comparisonMatrix[i - 1, j - 1] + 1;
+                        }
+                    }
+                    else
+                    {
+                        if (i - 1 < 0 || j - 1 < 0)
+                        {
+                            comparisonMatrix[i, j] = 0;
+                        }
+                        else
+                        {
+                            comparisonMatrix[i, j] = Math.Max(comparisonMatrix[i - 1, j], comparisonMatrix[i, j - 1]);
+                        }
+                    }
+
+                    if (comparisonMatrix[i, j] > maxVal)
+                    {
+                        maxVal = comparisonMatrix[i, j];
+                    }
+                }
+            }
+
+            return maxVal;
+        }
+    }
+}
+
---
+++ DynamicProgramming/DynamicProgrammingTests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using DynamicProgramming;
+using NuGet.Frameworks;
+
+namespace DynamicProgrammingTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        // Arrange
+        var string1 = "hello";
+        var string2 = "velo";
+        var string3 = "parcello";
+
+        // Act
+        var result1 = StringComparisonSolver.CalculateIsSame(string1, string2);
+        var result2 = StringComparisonSolver.CalculateIsSame(string1, string3);
+
+        // Assert
+        Assert.That(result1, Is.EqualTo(3));
+        Assert.That(result2, Is.EqualTo(4));
+    }
+}
